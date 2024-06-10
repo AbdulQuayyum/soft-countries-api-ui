789480f5ec2e3e7065147732dd2980455bb6c228
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2024-06-10 22:42:55 +0100

    Added Dummy Pages and made minor changes...

--- src/Views/Auth/SignIn.jsx
+++ src/Views/Auth/SignIn.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { DocumentTitle } from '../../Utilities/DocumentTitle'
 
 const SignInPage = () => {
---
+++ src/Views/General/Pricing.jsx
@@ -0,0 +1,11 @@
+import { DocumentTitle } from '../../Utilities/DocumentTitle'
+
+const PricingPage = () => {
+    DocumentTitle("Soft Countries API || Pricing Page")
+
+    return (
+        <div>PricingPage</div>
+    )
+}
+
+export default PricingPage
