371d3963b3b40e631c61d76f63902c5f2359ff10
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2024-07-05 17:50:06 +0100

    Worked on the hero section and made other changes...

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     const currentYear = new Date().getFullYear()
 
     return (
-        <div className='flex flex-col items-center justify-between w-full px-4 pt-4 pb-10 gap-y-4 md:flex-row'>
+        <div className='flex flex-col items-center justify-between max-w-[1400px] w-full pt-4 pb-10 gap-y-4 md:flex-row'>
             <span className="text-base text-black">{currentYear} &#169; Soft Countries API , All rights reserved</span>
             <span className="text-base text-black">Developed by   <a href="https://abdul-quayyum.vercel.app/" target="_blank" rel="noopener noreferrer">Abdul-Quayyum Alao</a>
             </span>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+import { TbArrowBigRightLines } from "react-icons/tb"
+
+import { FetchUserFlag } from '../APIs/request.api';
+
 const Hero = () => {
+    const [loading, setLoading] = useState(true);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [imageList, setImageList] = useState([]);
+    const [currentName, setCurrentName] = useState('');
+    const [position, setPosition] = useState('');
+
+    const FetchData = async () => {
+        try {
+            const response = await FetchUserFlag();
+            setImageList(response.data);
+
+            const randomIndex = Math.floor(Math.random() * response.data.length);
+            setCurrentImageIndex(randomIndex);
+            setCurrentName(response.data[randomIndex]?.name || '');
+
+            const positions = ['top-left', 'top-right', 'bottom-left', 'bottom-right'];
+            const randomPosition = positions[Math.floor(Math.random() * positions.length)];
+            setPosition(randomPosition);
+        } catch (error) {
+            console.error("Error fetching data: ", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        FetchData();
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const randomIndex = Math.floor(Math.random() * imageList.length);
+            setCurrentImageIndex(randomIndex);
+            setCurrentName(imageList[randomIndex]?.name || '');
+
+            const positions = ['top-left', 'top-right', 'bottom-left', 'bottom-right'];
+            const randomPosition = positions[Math.floor(Math.random() * positions.length)];
+            setPosition(randomPosition);
+        }, 10000);
+
+        return () => clearInterval(interval);
+    }, [imageList]);
+
+    const currentImageUrl = imageList[currentImageIndex]?.flag;
+
     return (
-        <div className="flex flex-col items-center justify-center h-full">
-            <div className="flex flex-col items-center justify-center gap-y-6">
-                <span className="text-3xl font-bold">Welcome to the Soft Countries API! </span>
-                <span className="text-lg font-bold">This API Service provides various datasets related to countries, including flags, codes, capitals, continents, and more. </span>
+        <div className='flex flex-col-reverse items-center justify-between w-full mt-0 gap-x-10 lg:gap-x-20 lg:mt-20 lg:flex-row'>
+            <div className="flex flex-col items-start justify-center max-w-xl gap-y-2 lg:gap-y-4 ">
+                <span className="text-4xl lg:text-6xl py-6 font-extrabold text-[#000]">Welcome to the Soft Countries API! </span>
+                <span className="text-base font-bold lg:text-lg">This API Service provides various datasets related to countries, including flags, codes, capitals, continents, and more. </span>
+            </div>
+            <div className='hero-img' style={{ backgroundImage: `url(${currentImageUrl})` }}>
+                <span className={`country-name ${position}`}>{currentName}</span>
             </div>
         </div>
     )
