25e31bfae3ad32e7dcd0c2efa9aceb4973b38942
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2024-07-07 17:46:40 +0100

    Added images...

--- src/Assets/Index.js
+++ src/Assets/Index.js
@@ -1,8 +1,12 @@
-import PageNotFound from "./png/404.png"
+import analytics from "./png/analytics.png"
+import data from "./png/data.png"
+import documentation from "./png/documentation.png"
+import easy from "./png/easy.png"
 import logo from "./png/logo.png"
 import map from "./png/map.png"
+import PageNotFound from "./png/404.png"
 
 
 import NoData from "./svg/NoData.svg"
 
-export default { logo, PageNotFound, map, NoData }
+export default { analytics, data, documentation, easy, logo, map, PageNotFound, NoData }
