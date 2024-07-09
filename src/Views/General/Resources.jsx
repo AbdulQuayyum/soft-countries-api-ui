f27e46b172d890ce559ae00c15c314f8c3d3b581
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2024-07-09 23:30:07 +0100

    Worked on the resources page...

--- src/Views/General/Resources.jsx
+++ src/Views/General/Resources.jsx
@@ -1,95 +1,19 @@
 import { useState } from 'react';
-import { Link, scroller } from 'react-scroll';
 import Select from 'react-select'
+import { Link, scroller } from 'react-scroll';
+import { useNavigate } from "react-router-dom"
+import { CopyBlock, monoBlue } from 'react-code-blocks';
 
 import { DocumentTitle } from '../../Utilities/DocumentTitle'
 import { customStyles02, replaceSpacingWithHyphen } from "../../Utilities/Utilities";
+import { sections, countryDatasets, steps, responseData, integrationExamplesData, integrationExamplesList } from '../../Data/Data';
 
 const ResourcesPage = () => {
-    const sections = [
-        { id: "introduction", Heading: "Introduction" },
-        { id: "getstarted", Heading: "Get Started" },
-        { id: "status", Heading: "Status" },
-        { id: "endpoints", Heading: "Endpoints" },
-        { id: "responses", Heading: "Responses" },
-        { id: "integration", Heading: "Integration" },
-        { id: "error", Heading: "Error Handling" },
-    ]
-
-    const countryDatasets = {
-        "africa-countries-id": {
-            description: "Provides data on African countries, including flags, country codes, and means of identification."
-        },
-        "africa-countries": {
-            description: "Provides general data about African countries."
-        },
-        "allcountries": {
-            description: "Provides general data about all countries."
-        },
-        "calling-code-flag": {
-            description: "Provides countries calling code, country name, country code and country flag."
-        },
-        "countries-capital": {
-            description: "Provides the capital cities of countries."
-        },
-        "countries-code": {
-            description: "Provides the calling codes of countries."
-        },
-        "countries-continent": {
-            description: "Lists countries and their respective continents."
-        },
-        "countries-name": {
-            description: "Provides a simple list of country names."
-        },
-        "countries-states": {
-            description: "Provides a list of countries and their states."
-        },
-        "countries": {
-            description: "Provides country names and their codes."
-        },
-        "country-flag-currency": {
-            description: "Provides country flags, names, codes, and currency details."
-        },
-        "country-flag": {
-            description: "Provides country flags and codes."
-        },
-        "country-with-flag": {
-            description: "Combines country names with their flags."
-        },
-        "nigeria-states-lga": {
-            description: "Provides data on states in Nigeria and their local governments."
-        },
-        "nigerian-plate-numbers": {
-            description: "Provides data on three letter codes on vehicle plate number and their local government in Nigeria."
-        },
-        "nigerian-states": {
-            description: "Provides data on states in Nigeria."
-        },
-        "west-africa-countries": {
-            description: "Provides data on countries in West Africa, including flags, codes, and means of identification."
-        }
-    };
-
-    const steps = [
-        {
-            title: "Create an Account",
-            description: "Sign up on our platform to create an account."
-        },
-        {
-            title: "Generate an API Key",
-            description: "After creating an account, generate your key from the dashboard. This key will be used to authenticate your requests."
-        },
-        {
-            title: "Select Mode",
-            description: "Choose between Test or Live mode. If you select Live mode, add the website URL to allow access."
-        },
-        {
-            title: "Make API Requests",
-            description: "Use your API key and your Username to make authenticated requests to the API endpoints."
-        }
-    ];
+    DocumentTitle("Soft Countries API || Resources Page")
+    const navigate = useNavigate()
 
     const [selectedOption, setSelectedOption] = useState(sections[0]);
+    const [selectedLanguage, setSelectedLanguage] = useState(integrationExamplesList[0]);
 
     const HandleSelectChange = (id) => {
         scroller.scrollTo(id, {
@@ -106,14 +30,17 @@ const ResourcesPage = () => {
         setSelectedOption(item);
     }
 
-    DocumentTitle("Soft Countries API || Resources Page")
+    const HandleSelectedLanguage = (selectedLanguage) => {
+        setSelectedLanguage(selectedLanguage);
+    };
+
     return (
         <section className="flex flex-col items-center justify-center w-full py-12 gap-y-12">
             <div className="flex flex-col items-center justify-center gap-y-2 lg:gap-y-4 ">
                 <span className="text-4xl lg:text-6xl text-center font-extrabold text-[#2E2C34]">Resources Page!</span>
                 <span className="text-xl text-[#667085] font-light text-center">This page contains resources that would help you understand out API Service better</span>
             </div>
-            <div className="container relative flex flex-col items-start justify-between gap-20 pt-4 pb-16 lg:flex-row">
+            <div className="container flex flex-col items-start justify-between gap-20 pt-4 lg:flex-row">
                 <div className="max-w-[278px] hidden lg:block w-full sticky z-10 top-40 rounded-xl h-auto ">
                     <h4 className=" text-lg font-bold uppercase text-[#2E2C34]">Content Outline</h4>
                     <ol className="flex flex-col w-full gap-4 py-6 text-lg tracking-wide rounded-md">
@@ -148,9 +75,9 @@ const ResourcesPage = () => {
                         onChange={HandleSelectedOption}
                     />
                 </div>
-                <section className="relative w-full mb-6 ml-0 border-b-2 custom-div md:mb-12">
+                <section className="w-full ml-0 custom-div">
                     <section id='introduction'>
-                        <h3 className='heading'>Welcome to the Soft Countries API</h3>
+                        <h3 className='heading'>Welcome to the Soft Countries API!</h3>
                         <div className='flex flex-col gap-y-2'>
                             <div className="subsection">
                                 <span>The Soft Countries API is a powerful and flexible API service that provides comprehensive datasets related to countries. You can access information such as flags, codes, capitals, continents, and more.</span>
@@ -161,7 +88,7 @@ const ResourcesPage = () => {
                         </div>
                     </section>
                     <section id='getstarted'>
-                        <h3 className='heading'>How to get started with Soft Countries API</h3>
+                        <h3 className='heading'>How to get started with Soft Countries API?</h3>
                         <div className="subsection">
                             <ul className='!list-disc pl-5 !mb-4'>
                                 {steps.map((step, index) => (
@@ -172,11 +99,8 @@ const ResourcesPage = () => {
                             </ul>
                         </div>
                     </section>
-                    <section id='status'>
-
-                    </section>
                     <section id='endpoints'>
-                        <h3 className='heading'>Get to know Soft Countries API available endpoints</h3>
+                        <h3 className='heading'>Get to know Soft Countries API available endpoints!</h3>
                         <div className='flex flex-col gap-y-2'>
                             <div className="subsection">
                                 <span>Soft Countries API comes with a number of country-related datasets.  </span>
@@ -205,16 +129,119 @@ const ResourcesPage = () => {
                             </div>
                         </div>
                     </section>
-                    <section id='responses'>
-
-                    </section>
                     <section id='integration'>
-
+                        <h3 className='heading'>How to integrate Soft Countries API endpoints?</h3>
+                        <div className='flex flex-col gap-y-2'>
+                            <div className="subsection">
+                                <span>Below are examples of how to integrate the endpoint using different programming languages. These examples demonstrate how to authenticate and fetch data from the API.</span>
+                            </div>
+                            <div className="flex flex-col w-full gap-y-4">
+                                <div className='flex justify-end w-full'>
+                                    <Select
+                                        isSearchable
+                                        value={selectedLanguage}
+                                        options={integrationExamplesList}
+                                        styles={customStyles02}
+                                        placeholder='Select option...'
+                                        getOptionLabel={(option) => option.Heading}
+                                        getOptionValue={(option) => replaceSpacingWithHyphen(option.Heading)}
+                                        onChange={HandleSelectedLanguage}
+                                    />
+                                </div>
+                                {selectedLanguage && (
+                                    <CopyBlock
+                                        text={integrationExamplesData[selectedLanguage.id]}
+                                        language={selectedLanguage.language}
+                                        showLineNumbers={false}
+                                        customStyle={{ padding: "12px", borderRadius: "8px" }}
+                                        theme={monoBlue}
+                                        copied={false}
+                                        wrapLines={true}
+                                        codeBlock={true}
+                                    />
+                                )}
+                            </div>
+                            <div className="subsection">
+                                <span>
+                                    These examples should help you get started with integrating the Soft Countries API into your application using different programming languages. If you need further assistance, don't hesitate to reach out to our support team.
+                                </span>
+                            </div>
+                        </div>
+                    </section>
+                    <section id='responses'>
+                        <h3 className='heading'>Get familiar with the responses of Soft Countries API endpoints!</h3>
+                        <div className="subsection">
+                            {Object.keys(responseData).map((key) => (
+                                <div key={key} className='flex flex-col py-4 gap-y-4 lg:max-w-2xl xl:max-w-4xl'>
+                                    <p className='text-lg font-extrabold'>{key}:</p>
+                                    <div>
+                                        <CopyBlock
+                                            text={JSON.stringify(responseData[key], null, 2)}
+                                            language="jsx"
+                                            showLineNumbers={false}
+                                            customStyle={{ padding: "12px 24px", borderRadius: "8px" }}
+                                            theme={monoBlue}
+                                            copied={false}
+                                            wrapLines={true}
+                                            codeBlock={true}
+                                        />
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     </section>
                     <section id='error'>
-
+                        <h3 className='heading'>Understand the errors of Soft Countries API endpoints!</h3>
+                        <div className="subsection">
+                            <span>The API provides meaningful error messages to help you understand what went wrong. Here are some common errors:</span>
+                        </div>
+                        <ul className='!list-disc pl-5 !mb-4'>
+                            <li className='py-2 gap-x-1'>
+                                <span className='block font-bold '>400 Bad Request:</span>
+                                <div className='flex flex-col mt-2 gap-y-1'>
+                                    <span>Your request is missing either or both Username, API key in the headers</span>
+                                    <span>Your request is missing the :type parameter</span>
+                                </div>
+                            </li>
+                            <li className='py-2 gap-x-1'>
+                                <span className='block font-bold '>401 Unauthorized:</span>
+                                <div className='flex flex-col mt-2 gap-y-1'>
+                                    <span>Your request has an invalid username or API key</span>
+                                    <span>Your request has an expired API key</span>
+                                </div>
+                            </li>
+                            <li className='py-2 gap-x-1'>
+                                <span className='block font-bold '>403 Forbidden: </span>
+                                <div className='flex flex-col mt-2 gap-y-1'>
+                                    <span>Your subscription has expired for pro or voulme subscribers</span>
+                                    <span>You are in test mode but trying to make requests on live domains</span>
+                                    <span>Your website domain is not added to to the allowed website list</span>
+                                    <span>Your monthly test call limit has been exceeded</span>
+                                </div>
+                            </li>
+                            <li className='py-2 gap-x-1'>
+                                <span className='block font-bold '>404 Not Found:</span>
+                                <div className='flex flex-col mt-2 gap-y-1'>
+                                    <span>The requested data type does not exist. make a request on pur contact us page</span>
+                                </div>
+                            </li>
+                            <li className='py-2 gap-x-1'>
+                                <span className='block font-bold '>500 Internal Server Error:</span>
+                                <div className='flex flex-col mt-2 gap-y-1'>
+                                    <span>An error occurred on the server.</span>
+                                </div>
+                            </li>
+                        </ul>
+                        <div className="subsection">
+                            <span>The error you are facing is not one of those above? Contact us!!!</span>
+                        </div>
                     </section>
                 </section>
+            </div>
+            <div className='mb-6'>
+                <button onClick={() => { navigate('/ContactUs') }} className='items-center px-6 flex cursor-pointer py-2.5 border border-[#2E2C34] rounded-lg'>
+                    Go to our Contact Us page
+                </button>
             </div>
         </section>
     )
