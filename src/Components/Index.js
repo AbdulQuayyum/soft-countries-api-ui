82a6c3bc7fdebda2255a484d652bfddb68029fd0
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2024-07-09 14:14:56 +0100

    Worked on the faq page and made minor change on contact us page...

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -1,7 +1,8 @@
+import Accordion from "./Accordion";
 import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./Hero";
 
 import AuthModal from "./Modals/AuthModal";
 
-export { Footer, Header, Hero, AuthModal };
+export { Accordion, Footer, Header, Hero, AuthModal };
