d35675326e23b45a9d4a654adb1cd26e8dac2180
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2025-05-10 20:49:50 +0100

    replaced depreciated icon from react icons....

--- src/Components/Dashboard/Sidebar.jsx
+++ src/Components/Dashboard/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { LuChevronsLeft, LuDoorOpen, LuLayoutGrid, LuSettings2, LuBarChartBig, LuUsers2 } from "react-icons/lu";
+import { LuChevronsLeft, LuDoorOpen, LuLayoutGrid, LuSettings2, LuBarChartBig, LuUsersRound } from "react-icons/lu";
 
 import { UseAuth } from "../../Contexts/Auth.Context";
 import assets from "../../Assets/Index";
@@ -49,7 +49,7 @@ const Sidebar = ({ showSidebar, setShowSidebar, isAdmin }) => {
                             {isAdmin &&
                                 <Link to="/UsersManagement" className={` flex items-center w-full p-4 gap-x-2 ${isActive('/UsersManagement')}`}>
                                     <span className="font-semibold md:block hidden text-lg text-[#2E2C34]">Users</span>
-                                    <LuUsers2 color="#2E2C34" size={20} />
+                                    <LuUsersRound color="#2E2C34" size={20} />
                                 </Link>
                             }
                             <Link to="/Statistics" className={` flex items-center w-full p-4 gap-x-2 ${isActive('/Statistics')}`}>
